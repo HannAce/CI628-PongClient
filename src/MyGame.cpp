28399ec097b7546b4f97bcfeda7ee1d41764f07c
Hannah Fisher <h.fisher5@uni.brighton.ac.uk>
2020-10-20 09:33:48 +0100

    Updated player movement and added ball

--- src/MyGame.cpp
+++ src/MyGame.cpp
@@ -18,19 +18,69 @@ void MyGame::send(std::string message) {
     messages.push_back(message);
 }
 
+// Controls to move the bat up and down
 void MyGame::input(SDL_Event& event) {
-    switch (event.key.keysym.sym) {
+    if (isPlayer1 && canPickPlayer) {
+        switch (event.key.keysym.sym) {
         case SDLK_w:
             send(event.type == SDL_KEYDOWN ? "W_DOWN" : "W_UP");
             break;
+        case SDLK_s:
+            send(event.type == SDL_KEYDOWN ? "S_DOWN" : "S_UP");
+            break;
+        }
+    }
+    else if (isPlayer2 && canPickPlayer) {
+        switch (event.key.keysym.sym) {
+        case SDLK_i:
+            send(event.type == SDL_KEYDOWN ? "I_DOWN" : "I_UP");
+            break;
+        case SDLK_k:
+            send(event.type == SDL_KEYDOWN ? "K_DOWN" : "K_UP");
+            break;
+        }
+    }
+    else { // Safety net
+        send("Not a valid player");
+    }
+
+    assignPlayer(event);
+}
+
+// assigns player 1 or 2 depending on input at start of game
+bool MyGame::assignPlayer(SDL_Event& event) {
+
+    if (canPickPlayer) {    
+        switch (event.key.keysym.sym) {
+        case SDLK_1:
+            isPlayer1 = true;
+            isPlayer2 = false;
+            canPickPlayer = false;
+
+            return isPlayer1;
+            break;
+
+        case SDLK_2:
+            isPlayer2 = true;
+            isPlayer1 = false;
+            canPickPlayer = false;
+
+            return isPlayer2;
+            break;
+        }
     }
 }
 
 void MyGame::update() {
     player1.y = game_data.player1Y;
+    player2.y = game_data.player2Y;
+    ball.x = game_data.ballX;
+    ball.y = game_data.ballY;
 }
 
 void MyGame::render(SDL_Renderer* renderer) {
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderDrawRect(renderer, &player1);
+    SDL_RenderDrawRect(renderer, &player2);
+    SDL_RenderDrawRect(renderer, &ball);
 }
