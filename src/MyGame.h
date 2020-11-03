76ce88fd2d6af70eabbbc21b0aa02f6c837848c5
Hannah Fisher <h.fisher5@uni.brighton.ac.uk>
2020-11-03 10:41:22 +0000

    Added load/draw texture functions and constructor

--- src/MyGame.h
+++ src/MyGame.h
@@ -5,7 +5,17 @@
 #include <vector>
 #include <string>
 
+#ifndef __MY_GAME_H__
+#define __MY_GAME_H__
+
+#include <iostream>
+#include <vector>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+#include <string>
+
 #include "SDL.h"
+
 
 static struct GameData {
     int player1Y = 0;
@@ -20,13 +30,28 @@ class MyGame {
     bool isPlayer2 = false;
     bool canPickPlayer = true;
 
-    private:
-        SDL_Rect player1 = { 200, 0, 20, 60 };
-        SDL_Rect player2 = { 580, 0, 20, 60 };
+private:
+    // Rectangles
+    SDL_Rect player1 = { 200, 0, 20, 60 };
+    SDL_Rect player2 = { 580, 0, 20, 60 };
 
-        SDL_Rect ball{ 0, 0, 20, 20 };
+    SDL_Rect ball = { 0, 0, 20, 20 };
 
-    public:
+    SDL_Rect scoreP1 = { 50, 50, 50, 50 };
+    SDL_Rect scoreP2 = { 0, 0, 500, 30 };
+
+    // Textures
+    SDL_Texture* batTexture;
+    SDL_Texture* ballTexture;
+    SDL_Texture* score0Texture;
+    SDL_Texture* score1Texture;
+    SDL_Texture* score2Texture;
+    SDL_Texture* score3Texture;
+
+public:
+    // default Constructor
+    MyGame(SDL_Renderer* renderer);
+
         std::vector<std::string> messages;
 
         void on_receive(std::string message, std::vector<std::string>& args);
@@ -35,7 +60,14 @@ class MyGame {
         void update();
         void render(SDL_Renderer* renderer);
 
+        // Player
         bool assignPlayer(SDL_Event& event);
+
+        // Textures
+        SDL_Texture* loadTexture(SDL_Renderer* renderer, std::string fileName);
+        void drawTexture(SDL_Renderer*, SDL_Texture* texture, SDL_Rect* dst, SDL_RendererFlip flip);
+        //void drawText(const std::string& text, const int& x, const int& y, TTF_Font* font);
+    
 };
 
 #endif
