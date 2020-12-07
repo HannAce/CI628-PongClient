af9eebf85a417ac6036bf0783f43aa62ae896379
Hannah Fisher <h.fisher5@uni.brighton.ac.uk>
2020-12-07 15:40:30 +0000

    Planned adding audio

--- src/MyGame.h
+++ src/MyGame.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <SDL_ttf.h>
 #include <SDL_image.h>
+#include <SDL_mixer.h>
 #include <string>
 
 #include "SDL.h"
@@ -25,11 +26,13 @@ class MyGame {
     bool isPlayer1 = false;
     bool isPlayer2 = false;
     bool canPickPlayer = true;
+    bool player1Win = false;
+    bool player2Win = false;
 
 private:
     // Rectangles
-    SDL_Rect player1 = { 200, 0, 20, 80 };
-    SDL_Rect player2 = { 580, 0, 20, 80 };
+    SDL_Rect player1 = { 200, 0, 20, 100 };
+    SDL_Rect player2 = { 580, 0, 20, 100 };
 
     SDL_Rect ball = { 0, 0, 15, 15 };
 
@@ -45,6 +48,9 @@ private:
 
     // Fonts
     TTF_Font* font1;
+
+    // Audio
+    
 
     // Colours
     Uint8 r;
@@ -70,12 +76,17 @@ public:
 
         // Player
         bool assignPlayer(SDL_Event& event);
+        bool playerWin();
 
         // Textures
+        // Texture and Text functions written with reference to Almas Baimagambetov, https://github.com/AlmasB/xcube2d, under the GNU General Public License v2.0
         SDL_Texture* loadTexture(SDL_Renderer* renderer, std::string fileName);
         void drawTexture(SDL_Renderer*, SDL_Texture* texture, SDL_Rect* dst, SDL_RendererFlip flip);
         void drawText(SDL_Renderer* renderer, const std::string& text, const int& x, const int& y, TTF_Font* font, SDL_Color colour);
         SDL_Texture* createTextureFromString(SDL_Renderer* renderer, const std::string& text, TTF_Font* font, SDL_Color colour);
+
+        // Audio
+        //void playSound();
     
 };
 
